256d818e2504eb08ac21a011a2d603e7e54e8c4d
ryancharris <harrisryan1@gmail.com>
2016-01-25 16:45:42 -0500

    Upload new bootstrap site

--- README.md
+++
@@ -1,3 +0,0 @@
-<h1>ryancharris.github.io</h1>
-
-<p>Personal Website</p>
--- js/rch.js
+++ js/rch.js
@@ -1,36 +1,39 @@
-var $overlay = $('<div id="overlay"></div>');
-var $container = $('<div class="videoContainer"></div>');
-var $wrapper = $('<div class="videoWrapper"></div>');
-var $video = $('<iframe>');
-
-$overlay.append($container)
-$container.append($wrapper);
-$wrapper.append($video);
-
-
-$("body").append($overlay);
-
-$("#gallery a").click( function (event) {
-
-	// Stop link direct
-	event.preventDefault();
-
-	// Get video location
-	var videoURL = $(this).attr("href");
-
-	// Parse video ID from URL
-	var vidNumber = videoURL.slice(17);
-
-	// Embed player using parsed ID
-	$video.attr("src", "http://player.vimeo.com/video/" + vidNumber);
-
-	// Fade in overlay
-	$overlay.fadeIn(500);
-
-
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top -50
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
 
-// Exit overlay when clicked
-$overlay.click(function () {
-	$(this).fadeOut(500);
-})
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Inserts videos into modal window
+$('.video-link').click(function(){
+	// Get URL from link
+	var videoURL = $(this).attr("href");
+
+	// Parse Vimeo vid number
+	var vidNumber = videoURL.slice(17);
+
+	// Get title from <img> alt
+	var videoTitle = $(this).children('img').attr('alt');
+
+	var videoCaption = $(this).parent().next().html();
+
+	// Insert video src into <iframe>
+	$('#video-frame').attr("src", "http://player.vimeo.com/video/" + vidNumber);
+
+	// Prepend title into modal header
+	$('.modal-title').replaceWith("<h4 class='modal-title' id='myModalLabel'>" + videoTitle + "</h4>");
+
+	// Insert caption into modal window
+	$('.modal-footer').replaceWith("<div class='modal-footer'>" + videoCaption + "</div>");
+
+});
